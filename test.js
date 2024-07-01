96bf231233163f74e23934d8511b3bd73ac40648
ryugotthis <ryucandoit6@gmail.com>
2024-07-01 14:16:17 +0900

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-console.log('Hi');
-console.log(document.getElementsByClassName('menu'));
-console.log(document.querySelector('.menu'));
-console.log(document.querySelectorAll('.menu'));
-
-let test = document.getElementById('test');
-console.log(test.innerText);
-console.log(test.textContent);
-console.log(test.innerHTML);
